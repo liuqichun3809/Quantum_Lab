1959d6ae7b3c063b05c69f449e16f916cd116cf2
liuqichun3809 <49220741+liuqichun3809@users.noreply.github.com>
2019-07-17 15:59:32 +0800

    Add files via upload

---
+++ qulab/drivers/DP800.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from qulab import BaseDriver, QInteger, QOption, QReal, QString, QVector
+
+
+class Driver(BaseDriver):
+
+    support_models = ['DP832']
+    '''Rigol DP800 series DC source'''
+
+    quants = [
+        QReal('Offset', value=0, unit='V', ch=1,
+          set_cmd=':SOUR%(ch)d:VOLT %(value).2f',
+          get_cmd=':SOUR%(ch)d:VOLT?'),
+
+         QOption('Output', ch=1,
+           set_cmd=':OUTP CH%(ch)d,%(option)s',
+           get_cmd=':OUTP? CH%(ch)d',
+           options=[('OFF', 'OFF'), ('ON', 'ON')]),
+    ]
