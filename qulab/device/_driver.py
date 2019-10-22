ffeabf7e9ec1a83aa01f77192fc6e3c230a387f9
LQC <liuqichun3809@163.com>
2019-10-22 19:37:35 +0800

    fix visa.query() error of GS200 current source

--- qulab/device/_driver.py
+++ qulab/device/_driver.py
@@ -233,6 +233,9 @@ zi_addr = re.compile(r'^(ZI)::([a-zA-Z]+[0-9]*)::([a-zA-Z-]+[0-9]*)(|::INSTR)$')
 pxi_addr = re.compile(r'^(PXI)[0-9]?::CHASSIS([0-9]*)::SLOT([0-9]*)::FUNC([0-9]*)::INSTR$')
 #其他类型 (OTHER)::(Key):(Value)::INSTR
 other_addr = re.compile(r'^(OTHER)::([a-zA-Z-]+):(.*)::INSTR$')
+# 横河仪器的GS系列yokogawa电流源不支持visa的query()
+# 该GS系列设备地址格式为 (GS)([0-9]*)::(.*)，例如 GS200::TCPIP::192.168.0.1，即格式中 (.*) 项为通常的 TCPIP 格式地址
+gs_addr = re.compile(r'^(GS)([0-9]*)::(.*)')
 
 def parse_resource_name(addr):
     m = p_addr.search(addr)
@@ -290,9 +293,18 @@ def _parse_other_resource_name(m, addr):
         addr=addr)
     return kw
 
+def _parse_gs_resource_name(m, addr):
+    rm = visa.ResourceManager('@ni')
+    type = m.group(1)
+    addr= m.group(3)
+    ins = rm.open_resource(addr)
+    company = 'YOKOGAWA'
+    model = m.group(1)+m.group(2)
+    return dict(type=type, ins=ins, company=company, model=model, addr=addr)
+
 def _parse_resource_name(addr):
     type = None
-    for addr_re in [ats_addr,zi_addr,pxi_addr,other_addr]:
+    for addr_re in [ats_addr,zi_addr,pxi_addr,other_addr,gs_addr]:
         m = addr_re.search(addr)
         if m is not None:
             type = m.group(1)
@@ -305,6 +317,8 @@ def _parse_resource_name(addr):
         return _parse_pxi_resource_name(m, addr)
     elif type == 'OTHER':
         return _parse_other_resource_name(m, addr)
+    elif type == 'GS':
+        return _parse_gs_resource_name(m, addr)
     else:
         return dict(type='Visa', addr=addr)
 
