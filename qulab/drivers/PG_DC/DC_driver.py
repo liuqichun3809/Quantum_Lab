187d68b4d91fc8426be9f0705ba01ca89bf1d1c6
LQC <liuqichun3809@163.com>
2019-11-14 20:01:45 +0800

    add PG_DC driver

--- qulab/drivers/PG_DC/DC_driver.py
+++ qulab/drivers/PG_DC/DC_driver.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import logging
 
-from qulab import BaseDriver, QInteger, QOption, QReal, QString, QVector
+from qulab.Driver import BaseDriver, QInteger, QOption, QReal, QString, QVector
 
 from .PG_DC_api import Voltage
 
+log = logging.getLogger('qulab.driver.PG_DC')
 
 class Driver(BaseDriver):
     support_models = ['PG_DC']
+
+    CHs=[1,2,3,4]
+
     quants = [
-        QReal('Offset', value=0, ch=0),
+        QReal('Offset', value=0, unit='V', ch=1),
             ]
 
-    def __init__(self, **kw):
-        BaseDriver.__init__(self, **kw)
-        self.ip=kw['IP']
+    def __init__(self, addr, **kw):
+        '''
+        addr: ip, e.g. '192.168.1.6'
+        '''
+        super().__init__(addr, **kw)
         self.model = 'PG_DC'
 
     def performOpen(self):
-        self.handle = Voltage(self.ip)
+        self.handle = Voltage(self.addr)
 
-    def performSetValue(self, quant, value, ch=0, **kw):
+    def performSetValue(self, quant, value, ch=1, **kw):
         if quant.name == 'Offset':
-            self.handle.setVoltage(value,ch=ch)
+            # ch: 1,2,3,4 -> 0,1,2,3
+            self.handle.setVoltage(value,ch=ch-1)
