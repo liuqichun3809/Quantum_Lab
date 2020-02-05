1afe2ffc5d51ee718aa7ed529640a2422aaa3849
liuqichun3809 <liuqichun3809@163.com>
2020-02-05 09:57:03 +0800

    add meas config

--- qulab/__init__.py
+++ qulab/__init__.py
@@ -13,3 +13,4 @@ from ._plot import imshow, make_figure_for_app, make_figures_for_App, plot
 from ._query import query
 from ._unit import unit
 from ._version import __version__
+from .meas_config import *
